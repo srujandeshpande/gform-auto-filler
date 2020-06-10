78895fd7458dec52f4751d76fc965953a1ecca00
Srujan Deshpande <srujan.deshpande@gmail.com>
2020-06-10 22:27:55 +0530

    Working and debugged for non ascii

--- automater.py
+++ automater.py
@@ -12,26 +12,25 @@ import pandas as pd
 
 option = webdriver.ChromeOptions()
 option.add_argument("-incognito")
-option.add_experimental_option("excludeSwitches", ['enable-automation']);
+#option.add_experimental_option("excludeSwitches", ['enable-automation']);
 #option.add_argument("--headless")
 #option.add_argument("disable-gpu");
 browser = webdriver.Chrome(executable_path=r'/home/srujan/chromedriver', chrome_options=option)
 
 
+def is_ascii(s):
+    return all(ord(c) < 128 for c in s)
 
 df1 = pd.read_csv('Indian-Female-Names.csv', delimiter=',')
 df2 = pd.read_csv('Indian-Male-Names.csv', delimiter=',')
-# User list comprehension to create a list of lists from Dataframe rows
 females = [list(row) for row in df1.values]
 males = [list(row) for row in df2.values]
 
-try:
-    for i in range(1):
-        #print(i,end=",")
+for i in range(20):
+    try:
         browser.get("https://docs.google.com/forms/d/e/1FAIpQLScPT1grJW3MifAPCm9TOS1X8C8dbRrhkiStXm1WC7OYwRa_Yg/viewform")
 
         text = browser.find_elements_by_class_name("quantumWizTextinputPaperinputInput");
-        print(len(text))
         rc = browser.find_elements_by_class_name("docssharedWizToggleLabeledLabelWrapper");
         submit = browser.find_element_by_class_name("appsMaterialWizButtonPaperbuttonContent")
 
@@ -43,25 +42,29 @@ try:
             ppsrn = '0'+ppsrn
         srn = "PES"+str(campus)+str(year)+str(ppsrn)
 
-        if(i%10):
-            n1 = random.choice(females)
-            n2 = random.choice(males)
-        else:
-            n2 = random.choice(females)
-            n1 = random.choice(males)
+        while(True):
+            if(i%10):
+                n1 = random.choice(females)
+                n2 = random.choice(males)
+            else:
+                n2 = random.choice(females)
+                n1 = random.choice(males)
+            if is_ascii(n1[0]) and is_ascii(n2[0]):
+                break
 
         name = n1[0]+' '+n2[0]
-        email = name.replace(' ','').replace('@','').replace('.','') + '@gmail.com'
+        email = name.replace(' ','').replace('@','').replace('.','').replace('/','') + '@gmail.com'
 
         text[0].send_keys(email)
         text[1].send_keys(name)
         text[2].send_keys(srn)
 
-        rc[campus].click()
+        rc[campus-1].click()
         rc[3].click()
 
+        submit.click()
+        print(i)
+    except:
+        pass
 
-        #submit.click()
-except:
-    browser.close()
 #browser.close()
